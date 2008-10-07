5cd43a11e63c8a1a85c7c4d7c5e61a5fe2aad80e
David Gleich <dgleich@stanford.edu>
2008-10-06 17:49:38 -0700

    Refactored test code with new planar graph test code.

--- test/test_planar.m
+++ test/test_planar.m
@@ -1,3 +1,29 @@
+%% Summary
+[G,X] = grid_graph(6,5);
+K5 = clique_graph(5);
+[is_planar ksubgraph EI]= boyer_myrvold_planarity_test(G);
+X = chrobak_payne_straight_line_drawing(G);
+test_planar_graph(G);
+K = kuratowski_subgraph(K5); % K5 has a Kuratowski subgraph
+A = make_maximal_planar(G);
+A = make_connected(G);
+A = make_biconnected_planar(G);
+p = planar_canonical_ordering(G);
+[p ei ej] = planar_canonical_ordering(G);
+is_kuratowski_graph(K);
+is_straight_line_drawing(G,X*20); % round to positions on a grid
+
+%% small empty graphs
+for i=0:3
+    A = sparse(i,i);
+    boyer_myrvold_planarity_test(A);
+    X= chrobak_payne_straight_line_drawing(A);
+    M = make_maximal_planar(A);
+    M = make_connected(A);
+    M = make_biconnected_planar(A);
+    is_kuratowski_graph(A);
+end
+
 %% boyer_myrvold_planarity_test
 if boyer_myrvold_planarity_test(clique_graph(5))
     error('matlab_bgl:test_planar','K_5 is not planar');
@@ -29,6 +55,24 @@ A = grid_graph(6,5);
 assert(is_planar==1,'matlab_bgl:testFailed','M(6,5) is planar');
 assert(isempty(K),'matlab_bgl:testFailed','no kuratowski for M(6,5)');
 
+% Try some interesting cases
+ntrials = 50; nxy=50; 
+for i=1:ntrials, 
+    x = rand(nxy,1); y=rand(nxy,1);
+    tri = delaunay(x,y);
+    S = sparse(tri(:,[1 1 2 2 3 3]),tri(:,[2 3 1 3 1 2]),1,nxy, nxy);    
+    % S is planar
+    [is_planar K EI]= boyer_myrvold_planarity_test(S);
+    assert(is_planar==1,'matlab_bgl:testFailed', 'Delaunay graphs are planar');
+    n= size(S,1); si= zeros(size(EI.edge_order)); 
+    for k=1:n, si(EI.vp(k):EI.vp(k+1)-1)=k; end
+    T = sparse(si,EI.edge_order,1);
+    assert(isequal(S~=0,T~=0),'matlab_bgl:testFailed', 'Incomplete Embedding Info');
+    S(1:5,1:5) = clique_graph(5);
+    is_planar= boyer_myrvold_planarity_test(S);    
+    assert(is_planar==0,'matlab_bgl:testFailed', 'Delaunay + K5 is not planar');
+end
+
 %% is_kuratowski_graph
 assert(is_kuratowski_graph(clique_graph(5))==1,'matlab_bgl:testFailed',...
     'K_5 is Kuratowski')
@@ -38,7 +82,7 @@ assert(is_kuratowski_graph(clique_graph([3,3]))==1,'matlab_bgl:testFailed',...
 rand('state',0);
 for i=1:10, 
     A=sprand(50,50,5/50); A=A|A'; [isp K]=boyer_myrvold_planarity_test(A);
-    if ~is_planar, assert(is_kuratowski_graph(K),'matlab_bgl:testFailed',...
+    if ~is_planar, assert(is_kuratowski_graph(K)==1,'matlab_bgl:testFailed',...
             'invalid Kuratowski graph'); end
 end
 
@@ -56,7 +100,6 @@ assert(is_straight_line_drawing(A,X)==1,'matlab_bgl:testFailed',...
 %     'K_4 on a square is not a straight line drawing');
 
 %% make_connected
-
 [ei ej] = make_connected(grid_graph(5));
 assert(isempty(ei) && isempty(ej), 'matlab_bgl:testFailed',...
     'the grid graph is already connected');
@@ -103,9 +146,8 @@ catch end
 
 % test 50 random planar graphs
 rand('state',0);
-ntrials = 50;
-for i=1:50, 
-    nxy = 50;
+ntrials = 50; nxy=50; 
+for i=1:ntrials, 
     x = rand(nxy,1); y=rand(nxy,1);
     tri = delaunay(x,y);
     S = sparse(tri(:,[1 1 2 2 3 3]),tri(:,[2 3 1 3 1 2]),1,nxy, nxy);
@@ -120,6 +162,36 @@ end
 %% chrobak_payne_straight_line_drawing
 G = grid_graph(10,1);
 X = chrobak_payne_straight_line_drawing(G);
+
+rand('state',0);
+ntrials = 50; nxy=50; 
+for i=1:ntrials, 
+    x = rand(nxy,1); y=rand(nxy,1);
+    tri = delaunay(x,y);
+    S = sparse(tri(:,[1 1 2 2 3 3]),tri(:,[2 3 1 3 1 2]),1,nxy, nxy);
+    X= chrobak_payne_straight_line_drawing(S);
+    A = make_maximal_planar(S);
+    [Y p ei ej]= chrobak_payne_straight_line_drawing(A,'is_maximal',0);    
+    assert(isempty(ei) && isempty(ej),'matlab_bgl:testFailed',...
+        'is_maximal=0 with maximal graph cannot generate additional edges');
+    [Y p ei ej]= chrobak_payne_straight_line_drawing(A,'is_maximal',1);
+    assert(isempty(ei) && isempty(ej),'matlab_bgl:testFailed',...
+        'is_maximal=1 cannot generate additional edges');
+    assert(is_straight_line_drawing(A,X)==1,'matlab_bgl:testFailed',...
+        'invalid drawing');
+    assert(is_straight_line_drawing(S,X)==1,'matlab_bgl:testFailed',...
+        'invalid drawing');
+    assert(is_straight_line_drawing(A,Y)==1,'matlab_bgl:testFailed',...
+        'invalid drawing');    
+    assert(is_straight_line_drawing(S,Y)==1,'matlab_bgl:testFailed',...
+        'invalid drawing');
+end
+
+
+%% planar_canonical_ordering(G)
+G = grid_graph(6,5);
+p = planar_canonical_ordering(G);
+
 
 %%
 % [is_planar ksubgraph EI]= boyer_mvrvold_planarity_test(G);
