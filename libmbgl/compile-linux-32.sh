c3d2e77426882d1222e71ca9676452d9854a24d2
David Gleich <dgleich@stanford.edu>
2008-10-12 18:24:30 -0700

    Working 32-bit linux and 64-bit linux codes

--- libmbgl/compile-linux-32.sh
+++ libmbgl/compile-linux-32.sh
@@ -7,7 +7,7 @@ source ccfiles.sh
 OFILES=`echo ${CCFILES} | sed -e 's/\.cc/\.o/g'`
 
 CFLAGS="-O2 -c -I${BOOST_DIR} -I${YASMIC_DIR}"
-CFLAGS="-g -c -I${BOOST_DIR} -I${YASMIC_DIR}"
+#CFLAGS="-g -c -I${BOOST_DIR} -I${YASMIC_DIR}"
 function echocmd {
 	echo $@
 	$@
