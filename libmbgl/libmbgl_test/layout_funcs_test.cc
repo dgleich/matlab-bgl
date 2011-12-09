60a527aa225ebf8164f53688506954b62fdbc6b9
David Gleich <dgleich@purdue.edu>
2011-12-08 21:29:21 -0500

    Fixes for layout

--- libmbgl/libmbgl_test/layout_funcs_test.cc
+++ libmbgl/libmbgl_test/layout_funcs_test.cc
@@ -20,15 +20,25 @@ int test_layout_1() {
   return 0;
 }
 
+int test_layout_fr() {
+  mbglIndex rp[] = {0, 3, 7, 11, 15, 19, 22, 26, 31, 36, 41, 46, 50, 54, 59, 64, 69, 74, 78, 82, 87, 92, 97, 102, 106, 109, 113, 117, 121, 125, 128};
+  mbglIndex ci[] = {0, 1, 6, 0, 1, 2, 7, 1, 2, 3, 8, 2, 3, 4, 9, 3, 4, 5, 10, 4, 5, 11, 0, 6, 7, 12, 1, 6, 7, 8, 13, 2, 7, 8, 9, 14, 3, 8, 9, 10, 15, 4, 9, 10, 11, 16, 5, 10, 11, 17, 6, 12, 13, 18, 7, 12, 13, 14, 19, 8, 13, 14, 15, 20, 9, 14, 15, 16, 21, 10, 15, 16, 17, 22, 11, 16, 17, 23, 12, 18, 19, 24, 13, 18, 19, 20, 25, 14, 19, 20, 21, 26, 15, 20, 21, 22, 27, 16, 21, 22, 23, 28, 17, 22, 23, 29, 18, 24, 25, 19, 24, 25, 26, 20, 25, 26, 27, 21, 26, 27, 28, 22, 27, 28, 29, 23, 28, 29};
+  double positions[60];
+
+  int rval=fruchterman_reingold_force_directed_layout(
+                30, ci, rp,
+                100, 10.0, 1,
+        50.0, 50.0, 0, positions);
+  myassert(rval==0,"function error");
+  return 0;
+}
+
 int layout_funcs_test() 
 {
   int nfail= 0, ntotal= 0, rval;
-  const char* name;
-
-  name= "kamada_kawai 3-grid";
-  rval= test_layout_1(); ntotal++;
-  if (rval!= 0) { nfail++; printf("%20s  %50s\n", name, errstr); }
-  else { printf("%20s  success\n", name); }
+  
+  testcase("kamada_kawai 3-grid", test_layout_1);
+  testcase("fr layout", test_layout_fr);
    
   printf("\n");
   printf("Total tests  : %3i\n", ntotal);
