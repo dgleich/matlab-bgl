9981cf68e96dedcfe5a9a5d62880488391e60bcf
David Gleich <dgleich@purdue.edu>
2012-01-08 15:47:47 -0500

    Initial cross-compiling makefile

---
+++ libmbgl/mingw64.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export CC=x86_64-pc-mingw32-gcc
+export CXX=x86_64-pc-mingw32-g++
+export CPP=x86_64-pc-mingw32-cpp
+export RANLIB=x86_64-pc-mingw32-ranlib
+export AR=x86_64-pc-mingw32-ar
